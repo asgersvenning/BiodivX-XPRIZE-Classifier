b9dec068bd29d3bec4626aaab8c44c06a52a799b
Asger Svenning <82270047+asgersvenning@users.noreply.github.com>
2024-05-30 17:40:34 +0200

    Create pipeline script placeholder

---
+++ pipeline.py
@@ -0,0 +1,3 @@
+### Placeholder for orchestrating the execution pipeline ###
+# We will run each of the dependency scripts as a subprocess or os.system
+# Script will also handle input-output formatting
